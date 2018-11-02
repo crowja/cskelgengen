039451a5039b9a4a1011c48617441d8617b2c15f
John Crow <crowja@gmail.com>
2018-11-02 14:00:56 -0500

    Updated to Doxygen-based tags timestamps.

--- skeletons/cutils/stamper.bash
+++ skeletons/cutils/stamper.bash
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-# Updates a "#define _TIMESTAMP" line in a file. Copy of original file
-# has the same name but suffixed with a "~", for example "myfile.c~".
+# Updates a Doxygen @date tag line in a file. Copy of the original
+# file has the same name but suffixed with a "~", for example
+# "myfile.c~".
 
 fname=$1
 
@@ -11,26 +12,24 @@ if [[ ! -f ${fname} || ! -r ${fname} || ! -w ${fname} ]]; then
   exit 1
 fi
 
-# Set the timestamp and the find and replace strings
-timestamp=`date +%s`
-fstr1="^[ ]*#[ ]*define [ ]*_TIMESTAMP$"
-rstr1="#define _TIMESTAMP "
+# Set the timestamp
+timestamp=$(date)
 
 # Copy the original file
 cp ${fname} ${fname}~
 
 # A little extra going on here, hence the two seds. The first sed
-# only acts when a "#define _TIMESTAMP" has no trailing text and
+# only acts when a Doxygen @date tag has no trailing text and
 # ends at the end of line; when that happens it simply tacks on a
-# single whitespace. The second sed looks for "#define _TIMESTAMP "
+# single whitespace. The second sed looks for "@date ..."
 # and replaces everything after the trailing whitespace with the new
-# timestamp text. In the unlikely event there's a similar macro
-# definition in the file (e.g. "#define _TIMESTAMP_FOO") it won't be
-# modified by this script. TODO Probably a cleaner way of doing this.
+# timestamp text. The replacements attempt to preserve indentation
+# and the choice of @date or \date. TODO Probably a cleaner way of
+# doing this.
 
 cat ${fname}~ \
-  | sed -e "s/${fstr1}/${rstr1} /" \
-  | sed -e "s/${rstr1} .*/#define _TIMESTAMP    ${timestamp}/" \
+  | sed -e "s/\([ ]*\*[ \*]*[@\\]date\)$/\1 /g" \
+  | sed -e "s/\([ ]*\*[ \*]*[@\\]date \).*/\1${timestamp}/g" \
   > ${fname}
 
 if [ $? -eq 0 ]; then
