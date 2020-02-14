4e9f1b83fa07c117ea0a418a2096c91cf6eb42b8
jac <crowja@gmail.com>
2020-02-13 21:22:22 -0600

    Updated import scripts.

--- skeletons/capp/import.bash
+++ skeletons/capp/import.bash
@@ -1,3 +1,7 @@
+#! /bin/bash
+
+wget -N -Psrc https://raw.githubusercontent.com/crowja/hodgepodge/master/errmsg.c
+wget -N -Psrc https://raw.githubusercontent.com/crowja/hodgepodge/master/errmsg.h
 wget -N -Psrc https://raw.githubusercontent.com/skeeto/optparse/master/optparse.h
 wget -N -Psrc https://raw.githubusercontent.com/crowja/linewrapper/master/linewrapper.c
 wget -N -Psrc https://raw.githubusercontent.com/crowja/linewrapper/master/linewrapper.h
---
+++ skeletons/cutils/import.bash
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+##wget -N -Psrc https://raw.githubusercontent.com/crowja/hodgepodge/master/errmsg.c
+##wget -N -Psrc https://raw.githubusercontent.com/crowja/hodgepodge/master/errmsg.h
