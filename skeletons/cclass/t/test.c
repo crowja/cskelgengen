98b01eb564c8ea698531f870bae10dbe97e35d83
jac <crowja@gmail.com>
2020-02-19 20:34:50 -0600

    Using tinyhelp.h.

--- skeletons/cclass/t/test.c
+++ skeletons/cclass/t/test.c
@@ -5,77 +5,30 @@
 #include <string.h>
 #include "TMPL_CLASS_NAME.h"
 #include "t/tinytest.h"
-
-#ifdef  COLOR_CODE
-#undef  COLOR_CODE
-#endif
-#define COLOR_CODE       0x1B
-
-#ifdef  COLOR_RED
-#undef  COLOR_RED
-#endif
-#define COLOR_RED        "[1;31m"
-
-#ifdef  COLOR_GREEN
-#undef  COLOR_GREEN
-#endif
-#define COLOR_GREEN      "[1;32m"
-
-#ifdef  COLOR_YELLOW
-#undef  COLOR_YELLOW
-#endif
-#define COLOR_YELLOW     "[1;33m"
-
-#ifdef  COLOR_RESET
-#undef  COLOR_RESET
-#endif
-#define COLOR_RESET      "[0m"
-
-static void
-printf_test_name(char *name, char *info)
-{
-   printf("%c%s%s%c%s", COLOR_CODE, COLOR_YELLOW, name, COLOR_CODE, COLOR_RESET);
-
-   if (NULL != info)
-      printf(" [%s]\n", info);
-   else
-      printf("\n");
-}
-
-static int
-two_doubles_equal(double x, double y)
-{
-   double      t = fabs(x) + fabs(y);
-   return fabs(x - y) < 4 * DBL_EPSILON * t ? 1 : 0;
-}
+#include "t/tinyhelp.h"
 
 static void
 test_constr(void)
 {
    struct TMPL_CLASS_NAME *z;
 
-   printf_test_name("test_constr", "TMPL_CLASS_TAG_new, TMPL_CLASS_TAG_free");
-
+   fprintf_test_info(stdout, "test_constr", "TMPL_CLASS_TAG_new, TMPL_CLASS_TAG_free");
    z = TMPL_CLASS_TAG_new();
    ASSERT("Constructor test", z);
    TMPL_CLASS_TAG_free(&z);
    ASSERT_EQUALS(NULL, z);
 }
 
-#if 0                                            /* 16 yy */
+#if 0                                            /* 12 yy */
 static void
 test_stub(void)
 {
    struct TMPL_CLASS_NAME *z;
-   double      x = 1.23;                    /* TODO */
 
-   _printf_test_name("test_stub", NULL);
-
+   fprintf_test_info(stdout, "test_stub", NULL);
    z = TMPL_CLASS_TAG_new();
    ASSERT("Constructor test, pt 1", z);
-   ASSERT("Here's a test ...", two_doubles_equal(x, 1.23));
    ASSERT_EQUALS(10, 10);
-
    TMPL_CLASS_TAG_free(&z);
    ASSERT_EQUALS(NULL, z);
 }
